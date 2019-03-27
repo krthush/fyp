da9a6caee790500569f5c7ff7bc8993a6e6755c0
krthush <33790334+krthush@users.noreply.github.com>
2019-03-27 20:56:45 +0000

    fixed potential select user bug

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -277,7 +277,9 @@
                                     @if (empty($project->selected_user_id))
                                     <a href="/projects/match/{{ $project->id }}/{{ $user->id }}"><button type="button" class="btn btn-outline-secondary" >Select User</button></a>
                                     @else
-                                    <a href="/projects/unmatch/{{ $project->id }}/{{ $user->id }}"><button type="button" class="btn btn-outline-secondary" >Deselect User</button></a>
+                                        @if ($project->selected_user_id == $user->id)
+                                        <a href="/projects/unmatch/{{ $project->id }}/{{ $user->id }}"><button type="button" class="btn btn-outline-secondary" >Deselect User</button></a>
+                                        @endif
                                     @endif
                                 </div>
                             </div>
