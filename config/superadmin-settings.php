f3b1093e2b6dcd481dc4b6ae81e34ce1c9157dce
krthush <33790334+krthush@users.noreply.github.com>
2019-05-22 04:35:07 +0100

    Project selection by students now working

--- config/superadmin-settings.php
+++ config/superadmin-settings.php
@@ -2,7 +2,7 @@
 
 return [
     'active_project_viewing' => true,
-    'active_project_selection' => true,
+    'active_project_selection' => false,
 	'active_project_first_matching' => false,
 	'active_project_all_matching' => false,
 ];
